94370ac3af7d5e15114ee798ddd9b76bfb4f40fa
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2026-03-29 16:30:19 +0700

    add validate script

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { add } from '@autospace/sample-lib';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `1 + 1 = ${add(1, 1)}`;
   }
 }
