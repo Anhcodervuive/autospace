9ce3862a11dc2e413d3f31734cd8fc718281ab08
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2026-03-30 14:54:51 +0700

    Config prisma, cors, graphQL.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://studio.apollographql.com',
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
