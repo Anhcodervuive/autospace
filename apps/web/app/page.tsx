dcbb45004ec8778dc7945de921523638bb4b4dac
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2026-04-10 14:09:24 +0700

    Add garages search filter schema

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,18 +4,20 @@ import { useQuery } from '@apollo/client/react';
 import { SearchGaragesDocument } from '@autospace/network/src/gql/generated';
 
 export default function Home() {
-  const { data: queryData } = useQuery(SearchGaragesDocument, { variables: {
-    dateFilter: { end: '2026-04-02', start: '2026-04-01' },
-    locationFilter: { 
-      ne_lat: 1,
-      ne_lng: 1,
-      sw_lat: -1,
-      sw_lng: -1,
-     },
-     slotsFilter: {
-      pricePerHour: { gte: 0 },
-     }
-  } });
+  const { data: queryData } = useQuery(SearchGaragesDocument, {
+    variables: {
+      dateFilter: { end: '2026-04-02', start: '2026-04-01' },
+      locationFilter: {
+        ne_lat: 1,
+        ne_lng: 1,
+        sw_lat: -1,
+        sw_lng: -1,
+      },
+      slotsFilter: {
+        pricePerHour: { gte: 0 },
+      },
+    },
+  });
 
   return (
     <main className="p-8">
