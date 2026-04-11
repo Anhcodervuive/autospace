a4c7848577f8a669f1b9e52765a1070172c45c81
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2026-04-11 22:16:10 +0700

    Set up map

--- apps/web/app/search/page.tsx
+++ apps/web/app/search/page.tsx
@@ -1,13 +1,13 @@
-'use client'
+'use client';
 
-import SearchPageLib from "@autospace/ui/components/templates/SearchPage"
+import SearchPageLib from '@autospace/ui/components/templates/SearchPage';
 
 const SearchPage = () => {
   return (
     <div>
       <SearchPageLib />
     </div>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
--- libs/ui/components/templates/SearchPage.tsx
+++ libs/ui/components/templates/SearchPage.tsx
@@ -4,11 +4,18 @@ import { Panel } from '../organisms/map/Panel'
 import { DefaultZoomControls } from '../organisms/map/ZoomControls'
 import { ViewStateChangeEvent } from 'react-map-gl/mapbox'
 import { initialViewState } from '@autospace/util/constants'
+import { SearchPlaceBox } from '../organisms/map/SearchPlacesBox'
 
 const SearchPage = () => {
   const handleMapChange = useCallback((target: ViewStateChangeEvent['target']) => {
     const bounds = target.getBounds()
-    console.log('Map bounds changed:', bounds)
+    const localFilter = {
+      net_lat: bounds?.getNorthEast().lat || 0,
+      net_lng: bounds?.getNorthEast().lng || 0,
+      sw_lat: bounds?.getSouthWest().lat || 0,
+      sw_lng: bounds?.getSouthWest().lng || 0,
+    }
+    console.log('Location filter', localFilter)
   }, [])
   return (
     <div>
@@ -18,6 +25,9 @@ const SearchPage = () => {
             onDragEnd={(e) => handleMapChange(e.target)}
             initialViewState={initialViewState}
         >
+            <Panel position='left-top'>
+          <SearchPlaceBox />
+            </Panel>
             <Panel position='right-center'>
                 <DefaultZoomControls />
             </Panel>
