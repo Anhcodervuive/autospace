593429a2f92a8ed4f93b63b081f131c86b9df285
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2026-04-26 11:25:46 +0700

    Fix bug stripe payment session

--- apps/web/app/booking-failed/page.tsx
+++ apps/web/app/booking-failed/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
-  return (
-    <div>Booking failed</div>
-  )
-}
+  return <div>Booking failed</div>;
+};
 
-export default page
+export default page;
