65156fd1127a8babe53289e64e790a2f5dffa9ed
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2026-04-26 19:36:18 +0700

    Add register company UI for normal user

--- apps/web-manager/app/page.tsx
+++ apps/web-manager/app/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { IsLoggedIn } from '@autospace/ui/components/organisms/IsLoggedIn';
+import { IsManager } from '@autospace/ui/components/organisms/IsManager';
 
 export default function Home() {
   return (
     <main>
-      <IsLoggedIn>{(uid) => <div>{uid}</div>}</IsLoggedIn>
+      <IsLoggedIn>
+        <IsManager>Hello manager</IsManager>
+      </IsLoggedIn>
     </main>
   );
 }
