f9d7b7e77b299e8bd38027c94d884aee28809415
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2026-05-03 21:23:04 +0700

    Delete non-using dependency for api

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { add } from '@autospace/sample-lib';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `1 + 1 = ${add(1, 1)}`;
+    return `1 + 1`;
   }
 }
